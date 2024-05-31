08106f8193ccc39f7f8eb3e84e8e334b77cf8585
Carlos-Baquero <carlosbg14@gmail.com>
2024-05-31 17:47:23 +0200

    trasladando las cards a su propio componente y creando los enlaces

--- quiz-app/src/App.vue
+++ quiz-app/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Quizes from "./data/quizes.json";
+import Card from "./components/Card.vue";
 import { ref, watch } from "vue";
 
 const quizes = ref(Quizes);
@@ -19,13 +20,7 @@ watch(search, () => {
       <input v-model.trim="search" type="text" placeholder="Search..." />
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img :src="quiz.img" alt="" />
-        <div class="card-text">
-          <h2>{{ quiz.name }}</h2>
-          <p>{{ quiz.questions.length }} questions</p>
-        </div>
-      </div>
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
     </div>
   </div>
 </template>
@@ -56,27 +51,5 @@ header input {
   display: flex;
   flex-wrap: wrap;
   margin-top: 40px;
-}
-
-/* CARD */
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-.card .card-text {
-  padding: 0 5px;
-}
-.card .card-text h2 {
-  font-weight: bold;
 }
 </style>
