87f8081124dece3fc7412a5bd4b0c79b340cb43b
Carlos-Baquero <carlosbg14@gmail.com>
2024-06-04 17:40:21 +0200

    first commit. creating router links and responsive car database

---
+++ router-car-app/src/components/Nav.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <RouterLink active-class="active" to="/">Home</RouterLink>
+    <RouterLink active-class="active" to="/about">About</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+.active {
+  font-size: 25px;
+  font-weight: 900;
+  color: wheat;
+}
+</style>
