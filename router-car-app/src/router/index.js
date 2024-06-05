9c84e9af7687522a045eb91d2ee367d40e2a3d07
Carlos-Baquero <carlosbg14@gmail.com>
2024-06-05 19:39:42 +0200

    añadiendo botón para la info de contacto y manejando url no existentes

--- router-car-app/src/router/index.js
+++ router-car-app/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import CarView from "../views/CarView.vue";
 import ContactView from "../views/ContactView.vue";
+import NotFoundView from "../views/404View.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,10 @@ const router = createRouter({
       path: "/",
       name: "Home",
       component: HomeView,
+    },
+    {
+      path: "/home",
+      redirect: "/", //Cuando un usuario llama a la ruta /home lo redirige a otro sito que hemos definido, en este caso la home page
     },
     {
       path: "/about",
@@ -27,6 +32,11 @@ const router = createRouter({
           component: ContactView,
         },
       ],
+    },
+    {
+      path: "/:catchall(.*)*", //Regular Expression para los paths que no hemos definido
+      name: "Not Found",
+      component: NotFoundView,
     },
   ],
 });
--- router-car-app/src/views/CarView.vue
+++ router-car-app/src/views/CarView.vue
@@ -1,18 +1,29 @@
 <script setup>
-import { useRoute, RouterView } from "vue-router";
+import { useRoute, useRouter, RouterView } from "vue-router";
 import cars from "../data/cars.json";
 
-const route = useRoute();
+const route = useRoute(); //Composable que nos permite extraer info
+const router = useRouter(); //Nos permite manipular la info y cambiar la ruta
+
+const carId = parseInt(route.params.id);
 
 const car = cars.find((c) => c.id === parseInt(route.params.id));
+
+const showContact = () => {
+  return router.push(`/cars/${carId}/contact`);
+};
 </script>
 
 <template>
-  <div>
+  <div v-if="car">
     <h1>Car view</h1>
     <p>{{ car.name }}</p>
     <p>{{ car.year }}</p>
     <p>{{ car.price }}</p>
+    <button @click="showContact">Contact info</button>
     <RouterView />
+  </div>
+  <div v-else>
+    <h1>Car not found</h1>
   </div>
 </template>
