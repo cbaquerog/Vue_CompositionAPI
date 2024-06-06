4192ce212b8d9fc285c5072bedce047447a24bdb
Carlos-Baquero <carlosbg14@gmail.com>
2024-06-06 18:17:02 +0200

    terminando el juego y añadiendo vuelta al menu principal

--- quiz-app/src/views/QuizView.vue
+++ quiz-app/src/views/QuizView.vue
@@ -1,42 +1,63 @@
+<script setup>
+import Question from "../components/Question.vue";
+import QuizHeader from "../components/QuizHeader.vue";
+import { useRoute } from "vue-router";
+import { ref, watch, computed } from "vue";
+import quizes from "../data/quizes.json";
+import Result from "../components/Result.vue";
+
+const route = useRoute();
+const quizId = parseInt(route.params.id);
+const quiz = quizes.find((q) => q.id === quizId);
+const currentQuestionIndex = ref(0);
+const numberOfCorrectAnswers = ref(0);
+const showResults = ref(false);
+
+const questionStatus = computed(() => {
+  return `${currentQuestionIndex.value}/${quiz.questions.length}`;
+});
+const barPercentage = computed(() => {
+  return `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`;
+});
+
+const onOptionSelected = (isCorrect) => {
+  if (isCorrect) {
+    numberOfCorrectAnswers.value++;
+  }
+  if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+    showResults.value = true;
+  }
+  currentQuestionIndex.value++;
+};
+
+/*El equivalente de realizar la función con watch en vez de computed es el siguiente
+const questionStatus = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+
+watch(
+  () => currentQuestionIndex.value,
+  () => {
+    questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+  }
+); */
+</script>
+
 <template>
   <div>
-    <header>
-      <h4>Question 1/3</h4>
-      <div class="bar">
-        <div class="completion"></div>
-      </div>
-    </header>
+    <QuizHeader
+      :questionStatus="questionStatus"
+      :barPercentage="barPercentage"
+    />
     <div>
-      <div class="question-container">
-        <h1 class="question">What is the chemical value of table salt?</h1>
-      </div>
-      <div class="options-container">
-        <div class="option">
-          <p class="option-label">A</p>
-          <div class="option-value">
-            <p>NaCL</p>
-          </div>
-        </div>
-      </div>
+      <Question
+        v-if="!showResults"
+        :question="quiz.questions[currentQuestionIndex]"
+        @selectOption="onOptionSelected"
+      />
+      <Result
+        v-else
+        :quizQuestionLength="quiz.questions.length"
+        :numberOfCorrectAnswers="numberOfCorrectAnswers"
+      />
     </div>
   </div>
 </template>
-
-<style scoped>
-header {
-  margin-top: 20px;
-}
-header h4 {
-  font-size: 30px;
-}
-.bar {
-  width: 300px;
-  height: 50px;
-  border: 3px solid bisque;
-}
-.completion {
-  height: 100%;
-  width: 0%;
-  background-color: bisque;
-}
-</style>
