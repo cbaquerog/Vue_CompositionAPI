0c5eb55e9a461b084a9e904990dfc6c199b63288
Carlos-Baquero <carlosbg14@gmail.com>
2024-06-10 22:16:55 +0200

    creating a search bar and using mapbox API to get town search suggestions

--- weather-app/src/views/HomeView.vue
+++ weather-app/src/views/HomeView.vue
@@ -1,5 +1,50 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        v-model="searchQuery"
+        @input="getSearchResults"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+      <ul
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapBoxSearchResults"
+      >
+        <li
+          v-for="searchResult in mapBoxSearchResults"
+          :key="searchResult.id"
+          class="py-2 cursor-pointer"
+        >
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const mapboxAPIKey =
+  "pk.eyJ1IjoiY2JhcXVlcm9nIiwiYSI6ImNseDlkcjk2NTJvcm4yanB2dWgzdWx4bHcifQ.Y_TxFzpppYLLY2QhZW0G6g";
+const searchQuery = ref("");
+const queryTimeOut = ref(null);
+const mapBoxSearchResults = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeOut.value);
+  queryTimeOut.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      const result = await axios.get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+      );
+      mapBoxSearchResults.value = result.data.features;
+      return;
+    }
+    mapBoxSearchResults.value = null;
+  }, 300);
+};
+</script>
