0b919f4f3f735f10aebd364a753797f921c5c84e
Carlos-Baquero <carlosbg14@gmail.com>
2024-06-11 11:21:36 +0200

    handling API errors

--- weather-app/src/views/HomeView.vue
+++ weather-app/src/views/HomeView.vue
@@ -12,13 +12,19 @@
         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
         v-if="mapBoxSearchResults"
       >
-        <li
-          v-for="searchResult in mapBoxSearchResults"
-          :key="searchResult.id"
-          class="py-2 cursor-pointer"
-        >
-          {{ searchResult.place_name }}
-        </li>
+        <p v-if="searchError">Sorry, something went wrong, please try again</p>
+        <p v-if="!serverError && mapBoxSearchResults.length === 0">
+          No results match your query, try a different term
+        </p>
+        <template v-else>
+          <li
+            v-for="searchResult in mapBoxSearchResults"
+            :key="searchResult.id"
+            class="py-2 cursor-pointer"
+          >
+            {{ searchResult.place_name }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
@@ -33,17 +39,23 @@ const mapboxAPIKey =
 const searchQuery = ref("");
 const queryTimeOut = ref(null);
 const mapBoxSearchResults = ref(null);
+const searchError = ref(null);
 
 const getSearchResults = () => {
   clearTimeout(queryTimeOut.value);
   queryTimeOut.value = setTimeout(async () => {
     if (searchQuery.value !== "") {
-      const result = await axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
-      );
-      mapBoxSearchResults.value = result.data.features;
+      try {
+        const result = await axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+        );
+        mapBoxSearchResults.value = result.data.features;
+      } catch {
+        searchError.value = true;
+      }
       return;
     }
+
     mapBoxSearchResults.value = null;
   }, 300);
 };
